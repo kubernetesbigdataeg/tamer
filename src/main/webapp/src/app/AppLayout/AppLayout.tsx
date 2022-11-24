3dbd8b398fda93dada3546d41a8d0fed4232f74f
Javi Roman <jroman.espinar@gmail.com>
2022-11-24 18:33:31 +0100

    Fix #16 logout and header tool

--- src/main/webapp/src/app/AppLayout/AppLayout.tsx
+++ src/main/webapp/src/app/AppLayout/AppLayout.tsx
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 import * as React from 'react';
 import { NavLink, useLocation, useHistory } from 'react-router-dom';
 import {
@@ -8,10 +26,25 @@ import {
   Page,
   PageHeader,
   PageSidebar,
-  SkipToContent
+  SkipToContent,
+  KebabToggle,
+  Avatar,
+  Dropdown,
+  DropdownGroup,
+  DropdownToggle,
+  DropdownItem,
+  PageHeaderTools,
+  PageHeaderToolsGroup,
+  PageHeaderToolsItem,
+  Button,
+  ButtonVariant
 } from '@patternfly/react-core';
 import { generateRoutes, IAppRoute, IAppRouteGroup } from '@app/routes';
 import logo from '@app/bgimages/Patternfly-Logo.svg';
+import imgAvatar from '@app/bgimages/imgAvatar.svg';
+import CogIcon from '@patternfly/react-icons/dist/esm/icons/cog-icon';
+import HelpIcon from '@patternfly/react-icons/dist/esm/icons/help-icon';
+import Cookies from "js-cookie";
 
 interface IAppLayout {
   children: React.ReactNode;
@@ -21,6 +54,29 @@ const AppLayout: React.FunctionComponent<IAppLayout> = ({ children }) => {
   const [isNavOpen, setIsNavOpen] = React.useState(true);
   const [isMobileView, setIsMobileView] = React.useState(true);
   const [isNavOpenMobile, setIsNavOpenMobile] = React.useState(false);
+  const [isUser, setIsUser] = React.useState("anonymous");
+  const [isKebabDropdownOpen, setKebabDropdownOpen] = React.useState(false);
+  const [isDropdownOpen, setDropdownOpen] = React.useState(false);
+  const [isLoged, setIsLoged] = React.useState(false);
+  const history = useHistory();
+
+  const parseJwt = (token) => {
+    try {
+      return JSON.parse(atob(token.split('.')[1]));
+    } catch (e) {
+      return null;
+    }
+  };
+
+  React.useEffect(() => {
+    let value = {};
+    value = Cookies.getJSON('tamer-userinfo');
+    if (value) {
+      setIsLoged(true);
+      setIsUser(value.username);
+    }
+  }, []);
+
   const onNavToggleMobile = () => {
     setIsNavOpenMobile(!isNavOpenMobile);
   };
@@ -29,6 +85,22 @@ const AppLayout: React.FunctionComponent<IAppLayout> = ({ children }) => {
   };
   const onPageResize = (props: { mobileView: boolean; windowSize: number }) => {
     setIsMobileView(props.mobileView);
+  };
+
+  const onKebabDropdownToggle = event => {
+    setKebabDropdownOpen(!isKebabDropdownOpen)
+  };
+
+  const onKebabDropdownSelect = event => {
+    setKebabDropdownOpen(!isKebabDropdownOpen);
+  };
+
+  const onDropdownSelect = event => {
+    setDropdownOpen(!isDropdownOpen);
+  };
+
+  const onDropdownToggle = event => {
+    setDropdownOpen(!isDropdownOpen);
   };
 
   function LogoImg() {
@@ -41,9 +113,90 @@ const AppLayout: React.FunctionComponent<IAppLayout> = ({ children }) => {
     );
   }
 
+  function dropDownOptions(history) {
+    const logout = () => {
+      Cookies.remove('tamer-auth');
+      // Reload page for activating login logic
+      window.location.reload();
+    };
+    return { logout };
+  }
+
+  const { logout } = dropDownOptions(history);
+
+  const kebabDropdownItems = [
+    <DropdownItem key="key1">
+      <CogIcon /> Settings
+    </DropdownItem>,
+    <DropdownItem key="key2">
+      <HelpIcon /> Help
+    </DropdownItem>
+  ];
+  const userDropdownItems = [
+    <DropdownGroup key="group 2">
+      <DropdownItem key="group 2 profile">My profile</DropdownItem>
+      <DropdownItem key="group 2 user" component="button">
+        User management
+      </DropdownItem>
+      <DropdownItem key="group 2 logout" onClick={logout}>Logout</DropdownItem>
+    </DropdownGroup>
+  ];
+
+  const headerTools = (
+    <PageHeaderTools>
+      <PageHeaderToolsGroup
+        visibility={{
+          default: 'hidden',
+          lg: 'visible'
+        }} /** the settings and help icon buttons are only visible on desktop sizes and replaced by a kebab dropdown for other sizes */
+      >
+        <PageHeaderToolsItem>
+          <Button aria-label="Settings actions" variant={ButtonVariant.plain}>
+            <CogIcon />
+          </Button>
+        </PageHeaderToolsItem>
+        <PageHeaderToolsItem>
+          <Button aria-label="Help actions" variant={ButtonVariant.plain}>
+            <HelpIcon />
+          </Button>
+        </PageHeaderToolsItem>
+      </PageHeaderToolsGroup>
+      <PageHeaderToolsGroup>
+        <PageHeaderToolsItem
+          visibility={{
+            lg: 'hidden'
+          }} /** this kebab dropdown replaces the icon buttons and is hidden for desktop sizes */
+        >
+          <Dropdown
+            isPlain
+            position="right"
+            onSelect={onKebabDropdownSelect}
+            toggle={<KebabToggle onToggle={onKebabDropdownToggle} />}
+            isOpen={isKebabDropdownOpen}
+            dropdownItems={kebabDropdownItems}
+          />
+        </PageHeaderToolsItem>
+        <PageHeaderToolsItem
+          visibility={{ default: 'hidden', md: 'visible' }} /** this user dropdown is hidden on mobile sizes */
+        >
+          <Dropdown
+            isPlain
+            position="right"
+            onSelect={onDropdownSelect}
+            isOpen={isDropdownOpen}
+            toggle={<DropdownToggle onToggle={onDropdownToggle}>{isUser}</DropdownToggle>}
+            dropdownItems={userDropdownItems}
+          />
+        </PageHeaderToolsItem>
+      </PageHeaderToolsGroup>
+      <Avatar src={imgAvatar} alt="Avatar image" />
+    </PageHeaderTools>
+  );
+
   const Header = (
     <PageHeader
       logo={<LogoImg />}
+      headerTools={headerTools}
       showNavToggle
       isNavOpen={isNavOpen}
       onNavToggle={isMobileView ? onNavToggleMobile : onNavToggle}
@@ -53,7 +206,8 @@ const AppLayout: React.FunctionComponent<IAppLayout> = ({ children }) => {
   const location = useLocation();
 
   const renderNavItem = (route: IAppRoute, index: number) => (
-    <NavItem key={`${route.label}-${index}`} id={`${route.label}-${index}`} isActive={route.path === location.pathname}>
+    <NavItem key={`${route.label}-${index}`} id={`${route.label}-${index}`}
+             isActive={route.path === location.pathname}>
       <NavLink exact={route.exact} to={route.path}>
         {route.label}
       </NavLink>
